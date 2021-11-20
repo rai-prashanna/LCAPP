36eece9203603a29f14fcc0395783998170256b2
rai-prashanna <prai931024@gmail.com>
2021-11-20 18:04:37 +0100

    created 8 threads for push and pop operation

--- assingment-1/deque.cpp
+++ assingment-1/deque.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>      /* printf, scanf, NULL */
-#include <stdlib.h>  
+#include <stdio.h> /* printf, scanf, NULL */
+#include <stdlib.h>
 #include <cstddef>
 #include <iostream>
 #include <thread>
@@ -11,7 +11,7 @@ class DQueue
 {
   const int leftSentinelValue = -2147483648;
   const int rightSentinelValue = 2147483647;
-  
+
   struct QNode
   {
     QNode *left, *right;
@@ -21,174 +21,152 @@ class DQueue
   QNode *leftSentinel, *rightSentinel;
 
 public:
-
   DQueue()
   {
-    leftSentinel= (QNode*) malloc(sizeof(QNode));
-    leftSentinel->val=leftSentinelValue;
-    leftSentinel->left=NULL;
-    // cout<< leftSentinel;
-    // cout<< "\n";
-    // cout<< leftSentinelValue;
-    //  cout<< "\n";
-    //create rightSentinelNode
-    rightSentinel= (QNode*) malloc(sizeof(QNode));
-    rightSentinel->val=rightSentinelValue;
-    rightSentinel->right=NULL;
-    // cout<< rightSentinel;
-    // cout<< "\n";
-    // cout<< rightSentinelValue;
-    // cout<< "\n";
+    leftSentinel = (QNode *)malloc(sizeof(QNode));
+    leftSentinel->val = leftSentinelValue;
+    leftSentinel->left = NULL;
+    rightSentinel = (QNode *)malloc(sizeof(QNode));
+    rightSentinel->val = rightSentinelValue;
+    rightSentinel->right = NULL;
 
     //connect leftSentinel Node with rightSentinel
-    leftSentinel->right=rightSentinel;
+    leftSentinel->right = rightSentinel;
     //connect rightSentinel Node with leftSentinel
-    rightSentinel->left=leftSentinel;
-// cout<<leftSentinel->right;
-// cout<<"\n";
-// cout<<rightSentinel->left;
-// cout<<"\n";
-
+    rightSentinel->left = leftSentinel;
   }
 
   void PushLeft(int val)
   {
-    __transaction_atomic{
+    __transaction_atomic
+    {
+      QNode *qn = (QNode *)malloc(sizeof(QNode));
+      qn->val = val;
+      //there is no any node in between left and right Sentinel
+      if (leftSentinel->right == rightSentinel)
+      {
+        leftSentinel->right = qn;
+        qn->left = leftSentinel;
+        qn->right = rightSentinel;
+        rightSentinel->left = qn;
+      }
 
-    
-    QNode *qn = (QNode*) malloc(sizeof(QNode));
-    qn->val=val;
-//    cout<<"value of new node \n";
-//    cout<<qn->val;
-//    cout<<"\n";
-    //there is no any node in between left and right Sentinel
-    if(leftSentinel->right==rightSentinel){
- //     cout<<"inside empty node \n";
-      leftSentinel->right=qn;
-      qn->left=leftSentinel;
-      qn->right=rightSentinel;
-      rightSentinel->left=qn;
-      //cout<<"left sentienel pointed \n";
-      //cout<<leftSentinel->right->val;
-      //cout<<"\n";
-    }
-    
-    //when there is node in between left and right sentinel
-    else{
-      //cout<<"inside non-empty node \n";
-      QNode *oldleftMostNode=leftSentinel->right;
-      leftSentinel->right=qn;
-      oldleftMostNode->left=qn;
-      qn->left=leftSentinel;
-      qn->right=oldleftMostNode;
-      //cout<<"left sentienel pointed to \n";
-      //cout<<leftSentinel->right->val;
-    }
+      //when there is node in between left and right sentinel
+      else
+      {
+        //cout<<"inside non-empty node \n";
+        QNode *oldleftMostNode = leftSentinel->right;
+        leftSentinel->right = qn;
+        oldleftMostNode->left = qn;
+        qn->left = leftSentinel;
+        qn->right = oldleftMostNode;
+      }
+      // cout << "left push new node of value \n";
+      // cout << leftSentinel->right->val;
+      // cout << "\n";
     }
   }
 
   void PushRight(int val)
   {
-    __transaction_atomic{
-    QNode *qn = (QNode*) malloc(sizeof(QNode));
-    qn->val=val;
-  //  cout<<"created new node with new value \n";
-    //cout<<qn->val;
-    //there is no any node in between left and right Sentinel
-    if(leftSentinel->right==rightSentinel){
-      //cout<<"empty nodes between \n";
+    __transaction_atomic
+    {
+      QNode *qn = (QNode *)malloc(sizeof(QNode));
+      qn->val = val;
+      //there is no any node in between left and right Sentinel
+      if (leftSentinel->right == rightSentinel)
+      {
+        //cout<<"empty nodes between \n";
 
-      leftSentinel->right=qn;
-      qn->left=leftSentinel;
-      qn->right=rightSentinel;
-      rightSentinel->left=qn;
-      //cout<<"right sentinel pointed to \n";
-      //cout<<rightSentinel->left->val;
-      //cout<<"\n";
-
-    }
-    else{
-      //cout<<"inside non-empty node\n";
-      QNode *oldrightMostNode=rightSentinel->left;
-      oldrightMostNode->right=qn;
-      qn->left=oldrightMostNode;
-      qn->right=rightSentinel;
-      rightSentinel->left=qn;
-      //cout<<"right sentinel pointed to \n";
-      //cout<<rightSentinel->left->val;
-      //cout<<"\n";
-      //cout<<"left sentinel pointed to \n";
-      //cout<<leftSentinel->right->val;
+        leftSentinel->right = qn;
+        qn->left = leftSentinel;
+        qn->right = rightSentinel;
+        rightSentinel->left = qn;
       }
+      else
+      {
+        //cout<<"inside non-empty node\n";
+        QNode *oldrightMostNode = rightSentinel->left;
+        oldrightMostNode->right = qn;
+        qn->left = oldrightMostNode;
+        qn->right = rightSentinel;
+        rightSentinel->left = qn;
+      }
+      // cout << "right push new node of value \n";
+      // cout << rightSentinel->left->val;
+      // cout << "\n";
     }
   }
 
   int PopLeft()
   {
-      __transaction_atomic {
+    __transaction_atomic
+    {
 
-    //if there is no any QNode in between rightSentinel and left Sentinel
-      if(leftSentinel->right==rightSentinel){
-      return -1;
-    }
-    //when there is atleast Qnode between rightSentinel and left Sentinel
-    else{
-      QNode *oldleftMostNode=leftSentinel->right;
-      leftSentinel->right=oldleftMostNode->right;
-      oldleftMostNode->right->left=leftSentinel;
-      return oldleftMostNode->val;
-    }
+      //if there is no any QNode in between rightSentinel and left Sentinel
+      if (leftSentinel->right == rightSentinel)
+      {
+        return -1;
+      }
+      //when there is atleast Qnode between rightSentinel and left Sentinel
+      else
+      {
+        QNode *oldleftMostNode = leftSentinel->right;
+        leftSentinel->right = oldleftMostNode->right;
+        oldleftMostNode->right->left = leftSentinel;
+        return oldleftMostNode->val;
+      }
+      // cout << "after left pop node of value \n";
+      // cout << leftSentinel->right->val;
+      // cout << "\n";
     }
   }
 
   int PopRight()
   {
-  __transaction_atomic {
+    __transaction_atomic
+    {
 
-    //if there is no any QNode in between rightSentinel and left Sentinel
-      if(leftSentinel->right==rightSentinel){
-      return -1;
-    }
-    //when there is atleast Qnode between rightSentinel and left Sentinel
-    else{
-      QNode *oldrightMostNode=rightSentinel->left;
-      oldrightMostNode->left->right=rightSentinel;
-      rightSentinel->left=oldrightMostNode->left;
-      return oldrightMostNode->val;
+      //if there is no any QNode in between rightSentinel and left Sentinel
+      if (leftSentinel->right == rightSentinel)
+      {
+        return -1;
+      }
+      //when there is atleast Qnode between rightSentinel and left Sentinel
+      else
+      {
+        QNode *oldrightMostNode = rightSentinel->left;
+        oldrightMostNode->left->right = rightSentinel;
+        rightSentinel->left = oldrightMostNode->left;
+        return oldrightMostNode->val;
+      }
+      // cout << "after right pop we have node of value \n";
+      // cout << rightSentinel->left->val;
+      // cout << "\n";
     }
   }
-  }
-
 };
 
 int main()
 {
-  int num_threads = 2;
+  int num_threads = 8;
   // create and join threads
   std::thread *t = new std::thread[num_threads];
   DQueue d;
-  for (int i = 0; i < num_threads; ++i)
-  {
-    // call void calcArea(int tn, int startidx, int num_trapezes_per_thread, double step)
-    //t[i] = std::thread(calcArea, i, num_threads, num_trapezes, step);
- t[i] = std::thread(&DQueue::PushLeft,d,4);
-  }
+  t[0] = std::thread(&DQueue::PushLeft, d, 3);
+  t[1] = std::thread(&DQueue::PushLeft, d, 4);
+  t[2] = std::thread(&DQueue::PushLeft, d, 5);
+  t[3] = std::thread(&DQueue::PushLeft, d, 6);
+
+  t[4] = std::thread(&DQueue::PopLeft, d);
+  t[5] = std::thread(&DQueue::PopRight, d);
+  t[6] = std::thread(&DQueue::PopLeft, d);
+  t[7] = std::thread(&DQueue::PopRight, d);
 
   for (int i = 0; i < num_threads; ++i)
   {
     t[i].join();
   }
-  d.PushRight(2);
-  d.PushRight(3);
-  d.PushRight(4);
-  d.PushLeft(5);
-  cout << "pop left with value \n";
-  cout << d.PopLeft();
-  cout << "\n";
-  cout << "pop right with value\n";
-  cout << d.PopRight();
-  cout << "\n";
-  cout << "pop right with value\n";
-  cout << d.PopRight();
+
   return 0;
 }
