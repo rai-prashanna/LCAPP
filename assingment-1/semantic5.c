69b0524dff98e72acb147f31530191114edcece9
rai-prashanna <prai931024@gmail.com>
2021-11-21 15:05:08 +0100

    Closed

--- assingment-1/semantic5.c
+++ assingment-1/semantic5.c
@@ -10,13 +10,14 @@ void nestedtIncrementByTwo()
 {
     __transaction_atomic
     {
-    __transaction_atomic{
-        update = update + 1;//second increment by inner transaction
+        __transaction_atomic
+        {
+            update = update + 1; //second increment by inner transaction
+        }
+        update = update + 1; //first increment by outer transaction
+
         __transaction_cancel;
         throw std::logic_error("abort transaction");
-    }
-    update=update+1;//first increment by outer transaction   
-
     }
 }
 
@@ -29,18 +30,17 @@ void nestedtIncrementByTwo()
 //         {
 
 //             update = update - 1;//2nd decrement by inner transaction
-           
+
 //         }
 //     }
 // }
 
 int main()
 {
-     std::thread t1(nestedtIncrementByTwo);
-     sleep(3);
-     t1.join();
+    std::thread t1(nestedtIncrementByTwo);
+    sleep(3);
+    t1.join();
 
     std::cout << " the value of update :: " << update << " \n ";
     return 0;
 }
-
