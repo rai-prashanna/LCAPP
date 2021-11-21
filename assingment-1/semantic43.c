b54761be5182ee3cf93b667fd22b322643fc3cec
rai-prashanna <prai931024@gmail.com>
2021-11-21 11:29:02 +0100

    semanitc 4.3

---
+++ assingment-1/semantic43.c
@@ -0,0 +1,48 @@
+#include <atomic>
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <atomic>
+
+int update = 2;
+
+void incrementByTwo()
+{
+    __transaction_atomic
+    {
+        update = update + 2;
+    }
+}
+
+void unitdecrement()
+{
+    try
+    {
+        __transaction_atomic
+        {
+            update = update - 1;
+
+            __transaction_cancel;
+             throw std::logic_error("abort transaction");
+        }
+    }
+    catch (std::exception &e)
+    {
+       // std::cout << "After exception the value of update :: " << update << " \n";
+    }
+}
+
+int main()
+{
+    std::thread t1(incrementByTwo);
+    std::thread t2(unitdecrement);
+    sleep(3);
+    t1.join();
+
+    //std::cout << "after unitincrement thread , the value of update :: " << update << " \n ";
+    t2.join();
+
+    std::cout << " the value of update :: " << update << " \n ";
+    return 0;
+}
+
