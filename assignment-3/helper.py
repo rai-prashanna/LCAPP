43f913c37db80d6780e7a6c59ae212a360c7133f
rai-prashanna <prai931024@gmail.com>
2021-12-28 12:29:18 +0100

    utility for subcategory of clothes

--- assignment-3/helper.py
+++ assignment-3/helper.py
@@ -1,10 +1,44 @@
 from datetime import datetime
 
-def lineSplitter(line):
-    givendatetime,itemid,sale=line.split(",")
-    date_time_obj=datetime.strptime(givendatetime, '%Y-%m-%d %H:%M')
-    if(date_time_obj.weekday()>4):
-        return (itemid,0), sale
+# def lineSplitterByWeek(line):
+#     givendatetime,itemid,sale=line.split(",")
+#     date_time_obj=datetime.strptime(givendatetime, '%Y-%m-%d %H:%M')
+#     if(date_time_obj.weekday()>4):
+#         # 0 for weekend
+#         return (itemid,0), sale
+#     else:
+#         # 1 for weekend
+#         return (itemid,1), sale
+#
+# def lineSplitterByMonth(line):
+#     givendatetime,itemid,sale=line.split(",")
+#     date_time_obj=datetime.strptime(givendatetime, '%Y-%m-%d %H:%M')
+#     return (itemid, date_time_obj.month), sale
 
-    else:
-        return (itemid,1), sale
+def getSubCategoryOfCategory(category):
+    rawoutputfile = open("sales_data/categories.csv", "r")
+    subcategories = []
+    next(rawoutputfile)
+    for line in rawoutputfile:
+        Category, Subcategory = line.split(',')
+        trimmedKey = Subcategory.rstrip("\n")
+        if(Category in subcategories):
+            subcategories.append(trimmedKey)
+        if(Category in category):
+            subcategories.append(trimmedKey)
+    return subcategories
+
+
+def getItemIdsFromSubCategory(subcategories):
+    rawoutputfile = open("sales_data/item_categories.csv", "r")
+    items = []
+    next(rawoutputfile)
+    for line in rawoutputfile:
+        itemid, itemname = line.split(',')
+        if(itemname.rstrip("\n") in subcategories):
+            items.append(itemid)
+    return items
+
+
+subclothes=getSubCategoryOfCategory("Clothes")
+getItemIdsFromSubCategory(subclothes)
